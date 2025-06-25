88bed439502e66f08ea8caac72e3edce043b8fc1
waled47d <dirbashiwalid@gmail.com>
2025-06-25 19:24:47 +0300

    fix: add GoogleOAuthProvider for login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 import { AuthProvider } from './contexts/AuthContext';
 import { LanguageProvider } from './contexts/LanguageContext';
 import { CartProvider } from './contexts/CartContext';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
+
 import Home from './pages/Home';
 import Products from './pages/Products';
 import Login from './pages/Login';
@@ -14,28 +18,30 @@ import Wallet from './pages/Wallet';
 
 function App() {
   return (
-    <LanguageProvider>
-      <AuthProvider>
-        <CartProvider>
-          <Router>
-            <div className="min-h-screen bg-dark-100 flex flex-col">
-              <Header />
-              <main className="flex-1">
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/products" element={<Products />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/register" element={<Register />} />
-                  <Route path="/dashboard" element={<Dashboard />} />
-                  <Route path="/wallet" element={<Wallet />} />
-                </Routes>
-              </main>
-              <Footer />
-            </div>
-          </Router>
-        </CartProvider>
-      </AuthProvider>
-    </LanguageProvider>
+    <GoogleOAuthProvider clientId="652588095833-i8les8t9c8t5dknp5c19tt9i8l46gq75.apps.googleusercontent.com">
+      <LanguageProvider>
+        <AuthProvider>
+          <CartProvider>
+            <Router>
+              <div className="min-h-screen bg-dark-100 flex flex-col">
+                <Header />
+                <main className="flex-1">
+                  <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/products" element={<Products />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/wallet" element={<Wallet />} />
+                  </Routes>
+                </main>
+                <Footer />
+              </div>
+            </Router>
+          </CartProvider>
+        </AuthProvider>
+      </LanguageProvider>
+    </GoogleOAuthProvider>
   );
 }
 
